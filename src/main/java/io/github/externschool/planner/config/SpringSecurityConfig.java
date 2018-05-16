d033b96655f0cca1f66c75151c9083d9645485cf
Dmytro Manzhula <d.manzhula@gmail.com>
2018-05-17 00:23:07 +0300

    #19:UserDetailService added

--- src/main/java/io/github/externschool/planner/config/SpringSecurityConfig.java
+++ src/main/java/io/github/externschool/planner/config/SpringSecurityConfig.java
@@ -1,6 +1,8 @@
 package io.github.externschool.planner.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,6 +11,20 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/").permitAll();
+        http.csrf().disable().authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/home").hasAnyRole().anyRequest().authenticated()
+        .and()
+                .formLogin().defaultSuccessUrl("/home.html", true)
+        .loginPage("/login").and().logout().permitAll();
+
+    }
+
+    //creating users
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authBuilder) throws Exception{
+        authBuilder.inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN");
     }
 }
