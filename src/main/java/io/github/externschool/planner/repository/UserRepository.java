57e5e90da0e95f6b1fe195f46544f1cd51dc4373
Dmytro Manzhula <d.manzhula@gmail.com>
2018-05-19 19:39:49 +0300

    user repository added

--- src/main/java/io/github/externschool/planner/repository/UserRepository.java
+++ src/main/java/io/github/externschool/planner/repository/UserRepository.java
@@ -2,6 +2,9 @@ package io.github.externschool.planner.repository;
 
 import io.github.externschool.planner.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
 }
