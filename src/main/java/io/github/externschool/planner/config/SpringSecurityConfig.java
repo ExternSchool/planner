de862728193f2043ddae197809a6a1978906d069
ben novikov <mailto.benkoff@gmail.com>
2018-05-20 14:23:35 +0300

    #35: Delete next issue code, Resolve app.properties conflict

--- src/main/java/io/github/externschool/planner/config/SpringSecurityConfig.java
+++ src/main/java/io/github/externschool/planner/config/SpringSecurityConfig.java
@@ -6,8 +6,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -15,26 +13,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/index", "/home", "/greeting",
-                        "/signup",
-                        "/js/**",
-                        "/css/**",
-                        "/img/**",
-                        "/webjars/**").permitAll()
-                .anyRequest().authenticated()
+        http.csrf().disable().authorizeRequests().anyRequest().fullyAuthenticated()
+                .antMatchers("/").permitAll()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout")
-                .permitAll();
+                .formLogin().defaultSuccessUrl("/", true)
+                .loginPage("/login").permitAll().and().logout().permitAll()
+                .and().httpBasic();
     }
 
     @Autowired
