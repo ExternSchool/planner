66cdd294314b724bb235bf861a15c5ee521c67c2
ben novikov <mailto.benkoff@gmail.com>
2018-05-21 07:28:19 +0300

    #20: Resolve conflict

---
+++ src/main/java/io/github/externschool/planner/entity/User.java
@@ -0,0 +1,96 @@
+package io.github.externschool.planner.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Transient
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "encrypted_password")
+    private String encryptedPassword;
+
+    public User(){
+
+    }
+
+    public User(String phoneNumber, String email, String password, String encryptedPassword) {
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(phoneNumber, user.phoneNumber) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(encryptedPassword, user.encryptedPassword);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, phoneNumber, email, password, encryptedPassword);
+    }
+}
