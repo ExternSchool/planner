1ef6050630e40524e768748a1b4cd78dd13c37cb
YatsunAnna1 <anny0-2011@mail.ru>
2018-05-22 17:36:31 +0200

    #24_Create_a_handler_that_will_receive_the_user's_registration_form_and_begin_the_process_of+registering_the_user

--- src/main/java/io/github/externschool/planner/controller/MainController.java
+++ src/main/java/io/github/externschool/planner/controller/MainController.java
@@ -1,14 +1,42 @@
 package io.github.externschool.planner.controller;
 
+import io.github.externschool.planner.entity.User;
+import io.github.externschool.planner.service.UserService;
+import io.github.externschool.planner.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class MainController {
+
+    private UserServiceImpl userService;
+
+
+    @Autowired
+    public MainController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
 
     @GetMapping({"", "/", "/index", "/home", "/greeting"})
     public String greeting() {
         return "greeting";
     }
+
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        userService.save(user);
+        return "redirect:/login";
+    }
+
+
 
 }
--- src/test/java/io/github/externschool/planner/controller/MainControllerTest.java
+++ src/test/java/io/github/externschool/planner/controller/MainControllerTest.java
@@ -1,35 +1,98 @@
 package io.github.externschool.planner.controller;
 
-
+import io.github.externschool.planner.entity.User;
+import io.github.externschool.planner.service.UserService;
+import io.github.externschool.planner.service.UserServiceImpl;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
+@SpringBootTest
+@AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 public class MainControllerTest {
-
+    @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    UserServiceImpl userService;
+
+
+    @Test
+    public void whenUserRegistration() {
+        User person = new User( "anna@yandex.ru", "password");
+        userService.save(person);
+//        Person result = this.persons.findByEmail(person.getEmail());
+//        assertThat(result.get/Roles().isEmpty(), is(true));
+    }
+
+    @Test
+    public void registerPOST() throws Exception
+    {
+        User user = new User("Anna", "pass");
+         userService.save(user);
+
+        mockMvc.perform(post("/registration")
+                .with(csrf())
+                .param("username", user.getEmail())
+                .param("password", user.getPassword()))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/login"));
+    }
+
+
+
 
 
     @Before
     public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new MainController()).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(new MainController(userService)).build();
     }
 
     @Test
-    public void testStartingPageReturnsGreetingView() throws Exception{
+    public void testIndex() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("greeting"))
                 .andDo(print());
     }
+
+    @Test
+    public void testLoginPageReturnsLoginFormView() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(content().string(Matchers.containsString("Login Form")));
+    }
+
+    @Test
+    public void testSecuredPageReturnsUnauthorized() throws Exception {
+        mockMvc.perform(get("/admin")).andExpect(status().isUnauthorized());
+    }
+
+
 }
