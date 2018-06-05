a93c0095d13dc7bce1d6b1c23798939d361466f1
Dmytro Manzhula <d.manzhula@gmail.com>
2018-06-06 00:05:06 +0300

    ticket to #57, tests added

--- src/main/java/io/github/externschool/planner/entity/profile/Teacher.java
+++ src/main/java/io/github/externschool/planner/entity/profile/Teacher.java
@@ -9,18 +9,41 @@ public class Teacher extends Person {
 
     private String schoolOfficer;
 
-    //Set<Subject> subjectList = new HashSet(); // in case of teaching more than one subject
-
-    //Set<ScheduleTime> teacherSchedule = new HashSet() // reception days/time
-
-    //constructor
-
-    //getters and setters
-
-    //
-
-
-
-
-
+//    Set<Subject> subjectList = new HashSet();
+//
+//
+//    public Teacher(String schoolOfficer, Set<Subject> subjectList) {
+//        this.schoolOfficer = schoolOfficer;
+//        this.subjectList = subjectList;
+//    }
+//
+//    public Teacher(Long id, User user, Long validationKey, String firstName, String patronymicName, String lastName, String phoneNumber, String password, String encryptedPassword, String schoolOfficer, Set<Subject> subjectList) {
+//        super(id, user, validationKey, firstName, patronymicName, lastName, phoneNumber, password, encryptedPassword);
+//        this.schoolOfficer = schoolOfficer;
+//        this.subjectList = subjectList;
+//    }
+//
+//    public String getSchoolOfficer() {
+//        return schoolOfficer;
+//    }
+//
+//    public void setSchoolOfficer(String schoolOfficer) {
+//        this.schoolOfficer = schoolOfficer;
+//    }
+//
+//    public Set<Subject> getSubjectList() {
+//        return subjectList;
+//    }
+//
+//    public void setSubjectList(Set<Subject> subjectList) {
+//        this.subjectList = subjectList;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Teacher{" +
+//                "schoolOfficer='" + schoolOfficer + '\'' +
+//                ", subjectList=" + subjectList +
+//                '}';
+//    }
 }
---
+++ src/test/java/io/github/externschool/planner/repository/profiles/StudentRepositoryTest.java
@@ -0,0 +1,50 @@
+package io.github.externschool.planner.repository.profiles;
+
+import io.github.externschool.planner.entity.profile.Gender;
+import io.github.externschool.planner.entity.profile.Student;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Test
+    public void shouldReturnListOfStudents(){
+        Student student1 = new Student();
+        student1.setFirstName("Marina");
+        student1.setGender(Gender.FEMALE);
+        entityManager.persist(student1);
+
+        Student student2 = new Student();
+        student2.setFirstName("Vasia");
+        student2.setGender(Gender.MALE);
+        entityManager.persist(student2);
+
+        List<Student> studentList = this.studentRepository.findAll();
+
+        System.out.println(studentList.get(0));
+
+        assertThat(studentList).isNotNull()
+                .hasSize(2)
+                .containsSubsequence(student1, student2);
+
+        assert(studentList.get(0).equals(student1));
+    }
+
+}
+
