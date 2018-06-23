e97d96e5cb4bad5b63f564e37d0a8f23155643b9
benkoff <mailto.benkoff@gmail.com>
2018-06-23 19:08:57 +0300

    #105 Refactor TeacherDTOToTeacher

--- src/main/java/io/github/externschool/planner/converter/TeacherDTOToTeacher.java
+++ src/main/java/io/github/externschool/planner/converter/TeacherDTOToTeacher.java
@@ -1,24 +1,25 @@
 package io.github.externschool.planner.converter;
 
-import io.github.externschool.planner.entity.User;
-import io.github.externschool.planner.entity.profile.Teacher;
 import io.github.externschool.planner.dto.TeacherDTO;
+import io.github.externschool.planner.entity.profile.Teacher;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TeacherDTOToTeacher implements Converter<TeacherDTO, Teacher> {
 
     @Override
-    public Teacher convert(TeacherDTO teacherDTO) {
-        return new Teacher(
-                teacherDTO.getId(),
-                new User(),
-                teacherDTO.getFirstName(),
-                teacherDTO.getPatronymicName(),
-                teacherDTO.getLastName(),
-                teacherDTO.getPhoneNumber(),
-                teacherDTO.getVerificationKey(),
-                teacherDTO.getOfficer(),
-                teacherDTO.getSchoolSubjects()
-        );
+    public Teacher convert(final TeacherDTO teacherDTO) {
+        Teacher teacher = new Teacher();
+        teacher.setId(teacherDTO.getId());
+        teacher.setVerificationKey(teacherDTO.getVerificationKey());
+        teacher.setFirstName(teacherDTO.getFirstName());
+        teacher.setPatronymicName(teacherDTO.getPatronymicName());
+        teacher.setLastName(teacherDTO.getLastName());
+        teacher.setPhoneNumber(teacherDTO.getPhoneNumber());
+        teacher.setOfficer(teacherDTO.getOfficer());
+        teacher.setSubjects(teacherDTO.getSchoolSubjects());
+
+        return teacher;
     }
 }
