e0ca6039ca58e1fb317eb827ec08a59eaf85eb96
benkoff <mailto.benkoff@gmail.com>
2018-06-29 23:08:49 +0300

    #90 Polishing

--- src/test/java/io/github/externschool/planner/bootstrapdata/BootstrapDataPopulatorTest.java
+++ src/test/java/io/github/externschool/planner/bootstrapdata/BootstrapDataPopulatorTest.java
@@ -2,14 +2,11 @@ package io.github.externschool.planner.bootstrapdata;
 
 import io.github.externschool.planner.entity.Role;
 import io.github.externschool.planner.entity.User;
-import io.github.externschool.planner.entity.profile.Person;
 import io.github.externschool.planner.repository.UserRepository;
 import io.github.externschool.planner.service.RoleService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -29,8 +26,6 @@ public class BootstrapDataPopulatorTest {
     public void shouldReturnExpectedUser_AfterPropertiesSet() throws Exception {
         String expectedEmail = "q@q";
         Role expectedRole = roleService.getRoleByName("ROLE_ADMIN");
-//        bootstrap.afterPropertiesSet();
-
         User actualUser = userRepository.findByEmail(expectedEmail);
 
         assertThat(actualUser)
