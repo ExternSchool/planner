0031284ff38c6eaf772be68d081e48882b686460
benkoff <mailto.benkoff@gmail.com>
2018-07-04 16:30:34 +0300

    #102 Refactor Controller, Templates. Add Tests

--- src/main/java/io/github/externschool/planner/controller/TeacherController.java
+++ src/main/java/io/github/externschool/planner/controller/TeacherController.java
@@ -1,21 +1,101 @@
 package io.github.externschool.planner.controller;
 
+import io.github.externschool.planner.dto.TeacherDTO;
+import io.github.externschool.planner.entity.profile.Teacher;
+import io.github.externschool.planner.service.SchoolSubjectService;
+import io.github.externschool.planner.service.TeacherService;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Controller
+@RequestMapping("/teacher")
 public class TeacherController {
+    private final TeacherService teacherService;
+    private final SchoolSubjectService subjectService;
+    private final ConversionService conversionService;
 
-    //TeacherServiceImpl teacherService;
-
-    @GetMapping({"/teacher/profile"})
-    public String teacher() {
-        return "teacher/teacher_profile";
+    public TeacherController(final TeacherService teacherService,
+                             final SchoolSubjectService subjectService,
+                             final ConversionService conversionService) {
+        this.teacherService = teacherService;
+        this.subjectService = subjectService;
+        this.conversionService = conversionService;
     }
 
-    @PostMapping({"/teacher/profile"})
-    public String save() {
-        return "/";
+    @GetMapping("/")
+    public ModelAndView findAll() {
+        return new ModelAndView(
+                "teacher/teacher_list",
+                "teachers",
+                teacherService.findAllByOrderByLastNameAsc().stream()
+                        .map(t -> conversionService.convert(t, TeacherDTO.class))
+                        .collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView findOne(@PathVariable("id") Long id) {
+        TeacherDTO teacherDTO = conversionService.convert(teacherService.findTeacherById(id), TeacherDTO.class);
+
+        return show(teacherDTO);
+    }
+
+    @GetMapping("/add")
+    public ModelAndView add() {
+        Teacher teacher = teacherService.saveOrUpdateTeacher(new Teacher());
+        TeacherDTO teacherDTO = conversionService.convert(teacher, TeacherDTO.class);
+
+        return show(teacherDTO);
+    }
+
+    @PostMapping(value = "/update", params = "action=save")
+    public ModelAndView save(@ModelAttribute("teacher") TeacherDTO teacherDTO, Model model) {
+        teacherService.saveOrUpdateTeacher(conversionService.convert(teacherDTO,Teacher.class));
+
+        return new ModelAndView("redirect:/teacher/");
+    }
+
+    @PostMapping(value = "/update", params = "action=newKey")
+    public ModelAndView newKey(@ModelAttribute("teacher") TeacherDTO teacherDTO, Model model) {
+        teacherDTO = newKey(teacherDTO);
+
+        return show(teacherDTO);
+    }
+
+    @GetMapping(value = "/update")
+    public ModelAndView cancel() {
+        return new ModelAndView("redirect:/teacher/");
+    }
+
+    @RequestMapping("/{id}/delete")
+    public ModelAndView delete(@PathVariable("id") Long id) {
+        teacherService.deleteTeacher(id);
+
+        return new ModelAndView("redirect:/teacher/");
+    }
+
+    private TeacherDTO newKey(TeacherDTO teacherDTO) {
+        //TODO add key change confirmation request
+        //TODO move it to a key service
+        teacherDTO.setVerificationKey(UUID.randomUUID().toString());
+
+        return teacherDTO;
+    }
+
+    private ModelAndView show(TeacherDTO teacherDTO) {
+        ModelAndView modelAndView = new ModelAndView("teacher/teacher_profile");
+        modelAndView.addObject("teacher", teacherDTO);
+        modelAndView.addObject("allSubjects", subjectService.findAllByOrderByNameAsc());
+
+        return modelAndView;
     }
 }
---
+++ src/test/java/io/github/externschool/planner/controller/TeacherControllerIntegrationTest.java
@@ -0,0 +1,149 @@
+package io.github.externschool.planner.controller;
+
+import io.github.externschool.planner.dto.TeacherDTO;
+import io.github.externschool.planner.entity.profile.Teacher;
+import io.github.externschool.planner.service.TeacherService;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TeacherControllerIntegrationTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private TeacherService teacherService;
+    @Autowired
+    private ConversionService conversionService;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @WithMockUser(roles="ADMIN")
+    public void shouldReturnTeacherListTemplate_WhenGetRequestRootWithAdminRole() throws Exception {
+        mockMvc.perform(get("/teacher/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("teacher/teacher_list"))
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(content().string(Matchers.containsString("Teachers List")));
+    }
+
+    //TODO Think about should a Teacher have access to /teacher/ folder when has no access to the link in the header
+    @Test
+    @WithMockUser(roles="TEACHER")
+    public void shouldReturnTeacherListTemplate_WhenRequestWithTeacherRole() throws Exception {
+        mockMvc.perform(get("/teacher/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("teacher/teacher_list"))
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(content().string(Matchers.containsString("Teachers List")));
+    }
+
+    @Test
+    @WithMockUser(roles="GUEST")
+    public void shouldReturnForbidden_WhenRequestUnauthorized() throws Exception {
+        mockMvc.perform(get("/teacher/"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles="ADMIN")
+    public void shouldReturnModelAndView_WhenGetRequestTeacherId() throws Exception {
+        TeacherDTO teacherDTO = conversionService.convert(teacherService.findAllTeachers().get(0), TeacherDTO.class);
+        Long id = teacherDTO.getId();
+
+        mockMvc.perform(get("/teacher/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(view().name("teacher/teacher_profile"))
+                .andExpect(model().attribute("teacher", teacherDTO))
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(content().string(Matchers.containsString("Teacher Profile")));
+    }
+
+    @Test
+    @WithMockUser(roles="ADMIN")
+    public void shouldReturnModelAndView_WhenGetRequestTeacherAdd() throws Exception {
+        mockMvc.perform(get("/teacher/add"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("teacher/teacher_profile"))
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(content().string(Matchers.containsString("Teacher Profile")));
+    }
+
+    @Test
+    @WithMockUser(roles="ADMIN")
+    public void shouldRedirectToTeacherList_WhenPostRequestUpdateSave() throws Exception {
+        mockMvc.perform(post("/teacher/update").param("action", "save"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/teacher/"));
+    }
+
+    @Test
+    @WithMockUser(roles="ADMIN")
+    public void shouldReturnNewKey_WhenPostRequestUpdateNewKey() throws Exception {
+        TeacherDTO teacherDTO = conversionService.convert(teacherService.findAllTeachers().get(0), TeacherDTO.class);
+
+        mockMvc.perform(post("/teacher/update").param("action", "newKey"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("teacher/teacher_profile"))
+                .andExpect(model().attribute("teacher",
+                        Matchers.hasProperty("verificationKey",
+                                Matchers.not(teacherDTO.getVerificationKey()))))
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(content().string(Matchers.containsString("Teacher Profile")));
+    }
+
+    @Test
+    @WithMockUser(roles="ADMIN")
+    public void shouldRedirectToTeacherList_WhenGetRequestUpdate() throws Exception {
+        mockMvc.perform(get("/teacher/update"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/teacher/"));
+    }
+
+    @Test
+    @WithMockUser(roles="ADMIN")
+    public void shouldRedirectToTeacherList_WhenRequestDelete() throws Exception {
+        List<Teacher> teachers = teacherService.findAllTeachers();
+        Integer sizeBefore = teachers.size();
+        TeacherDTO teacherDTO = conversionService.convert(teachers.get(0), TeacherDTO.class);
+        Long id = teacherDTO.getId();
+
+        mockMvc.perform(get("/teacher/{id}/delete", id))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/teacher/"));
+
+        assertThat(teacherService.findAllTeachers().size()).isEqualTo(sizeBefore - 1);
+    }
+}
