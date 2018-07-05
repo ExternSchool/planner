0db47176a13aacb19acb6c9f2efd977f70fc14ca
benkoff <mailto.benkoff@gmail.com>
2018-07-05 22:44:56 +0300

    #144 Fix Formatting

--- src/main/java/io/github/externschool/planner/aop/LoggingAspect.java
+++ src/main/java/io/github/externschool/planner/aop/LoggingAspect.java
@@ -1,49 +1,47 @@
 package io.github.externschool.planner.aop;
 
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.aspectj.lang.reflect.MethodSignature;
-
 
 import java.util.Arrays;
 
 @Aspect
 @Component
 public class LoggingAspect {
-    private Logger log = LoggerFactory.getLogger(getClass());
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Pointcut("within(io.github.externschool.planner.controller..*) || within(io.github.externschool.planner.service..*) ")
-    private void allMethods() {
+    private void allMethods() {}
 
-    }
-
-        @Before("allMethods()")
-    public void log(JoinPoint joinPoint) {
+    @Before("allMethods()")
+    public void log(final JoinPoint joinPoint) {
         Object[] arguments = joinPoint.getArgs();
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
         System.out.println("before" + className + "." + methodName + "() -----");
-
-        for (int i = 0; i < arguments.length; i++) {
-            System.out.println(arguments[i]);
+        for (final Object argument : arguments) {
+            System.out.println(argument);
         }
     }
+
     @Around("allMethods()")
-    public Object logBusinessMethods(ProceedingJoinPoint call) throws Throwable {
+    public Object logBusinessMethods(final ProceedingJoinPoint call) throws Throwable {
         if (!log.isDebugEnabled()) {
             return call.proceed();
         } else {
-
             Object[] args = call.getArgs();
             String message = call.toShortString();
             log.info("{} called with args '{}'!", message, Arrays.deepToString(args));
             Object result = null;
-
 
             try {
                 result = call.proceed();
@@ -60,16 +58,8 @@ public class LoggingAspect {
         }
     }
 
-
-  @After("allMethods()")
-   public void logAfter(JoinPoint joinPoint) {
-            log.info("after: "+joinPoint.getTarget().getClass().getSimpleName()+" "+joinPoint.getSignature().getName());
-
+    @After("allMethods()")
+    public void logAfter(final JoinPoint joinPoint) {
+        log.info("after: "+joinPoint.getTarget().getClass().getSimpleName()+" "+joinPoint.getSignature().getName());
     }
-
-
 }
-
-
-
-
