1274288d2ec025287664e2ba80160db8ddb9b6ae
benkoff <mailto.benkoff@gmail.com>
2018-07-21 01:48:43 +0300

    Remove Unused Imports

--- src/main/java/io/github/externschool/planner/converter/TeacherDTOToTeacher.java
+++ src/main/java/io/github/externschool/planner/converter/TeacherDTOToTeacher.java
@@ -2,19 +2,11 @@ package io.github.externschool.planner.converter;
 
 import io.github.externschool.planner.dto.TeacherDTO;
 import io.github.externschool.planner.entity.profile.Teacher;
-import io.github.externschool.planner.service.TeacherService;
-import io.github.externschool.planner.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TeacherDTOToTeacher implements Converter<TeacherDTO, Teacher> {
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private TeacherService teacherService;
-
     @Override
     public Teacher convert(final TeacherDTO teacherDTO) {
         Teacher teacher = new Teacher();
