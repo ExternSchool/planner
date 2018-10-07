d3801e258fda03fd57fe73dbab4635e503b4002b
user.email <d.manzhula@gmail.com>
2018-10-08 00:38:16 +0300

    207-email service

---
+++ src/main/java/io/github/externschool/planner/emailservice/EmailService.java
@@ -0,0 +1,26 @@
+package io.github.externschool.planner.emailservice;
+
+import io.github.externschool.planner.entity.schedule.ScheduleEvent;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailService {
+
+    public JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendCancelEventMail(ScheduleEvent scheduleEvent){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setTo(scheduleEvent.getOwner().getEmail());
+        simpleMailMessage.setSubject("Event cancellation");
+        simpleMailMessage.setFrom("extern.school@gmail.com");
+        simpleMailMessage.setText("Sorry, may be next time)");
+        mailSender.send(simpleMailMessage);
+    }
+}
---
+++ src/test/java/io/github/externschool/planner/emailservice/EmailServiceTest.java
@@ -0,0 +1,37 @@
+package io.github.externschool.planner.emailservice;
+
+import io.github.externschool.planner.entity.User;
+import io.github.externschool.planner.entity.schedule.ScheduleEvent;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class EmailServiceTest {
+
+    @Autowired
+    private EmailService emailService;
+
+    private ScheduleEvent scheduleEvent;
+    private User user;
+
+    @Before
+    public void setup(){
+        user = new User();
+        user.setEmail("test@receiver.com");
+
+        scheduleEvent = new ScheduleEvent();
+        scheduleEvent.setOwner(user);
+        scheduleEvent.setCancelled(true);
+    }
+
+    @Test
+    public void shouldSendCancellationMail(){
+
+        emailService.sendCancelEventMail(scheduleEvent);
+    }
+}
